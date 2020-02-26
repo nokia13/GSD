48eb7b645ae3562a617c2daa98a23919089ae4e0
nokia13 <nokia13@users.noreply.github.com>
2020-02-26 11:59:58 +0100

    Initial commit

---
+++ web/script/shrinkscript.js
@@ -0,0 +1,28 @@
+var ipad = window.matchMedia("(min-width: 768px)");
+var fullscreen = window.matchMedia("(min-width: 1080px)");
+
+function myFunction() {
+  if (ipad.matches) {
+    scrollFunction("0px", "130px");
+  } else if (fullscreen.matches) {
+    scrollFunction("1000px", "1000px");
+  } else {
+    scrollFunction("0px", "220px");
+  }
+}
+
+function scrollFunction(x, y) {
+  console.log("scrollFunction x value = " + x);
+  if (document.documentElement.scrollTop > 80) {
+    document.getElementById("shrinktext").style.height = x;
+  } else {
+    document.getElementById("shrinktext").style.height = y;
+  }
+}
+
+window.onscroll = function() {
+  myFunction();
+};
+
+myFunction(); // Call listener function at run time
+fullscreen.addListener(myFunction); // Attach listener function on state changes
