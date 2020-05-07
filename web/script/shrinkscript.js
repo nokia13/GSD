b21d8697ba36b8fe3b9e61c7992f6abbe8b0e7ed
nokia13 <nokia13@users.noreply.github.com>
2020-05-07 15:37:45 +0200

    fixat css för dropdown
    nu kan den animeras ner

--- web/script/shrinkscript.js
+++ web/script/shrinkscript.js
@@ -1,31 +1,28 @@
-let ipad = window.matchMedia("(min-width: 768px)");
-let fullscreen = window.matchMedia("(min-width: 1080px)");
+let ipad = window.matchMedia('(min-width: 768px)')
+let fullscreen = window.matchMedia('(min-width: 1080px)')
 
-function myFunction() {
+function myFunction () {
   if (fullscreen.matches) {
-    scrollFunction("0px", "90px");
-  }
-  else if (ipad.matches) {
-    scrollFunction("0px", "130px");
-  }
-
-  else {
-    scrollFunction("0px", "220px");
-  }
-}
-
-function scrollFunction(x, y) {
-  console.log("scrollFunction x value = " + x);
-  if (document.documentElement.scrollTop > 80) {
-    document.getElementById("shrinktext").style.height = x;
+    scrollFunction('0px', '90px')
+  } else if (ipad.matches) {
+    scrollFunction('0px', '130px')
   } else {
-    document.getElementById("shrinktext").style.height = y;
+    scrollFunction('0px', '220px')
   }
 }
 
-window.onscroll = function() {
-  myFunction();
-};
+function scrollFunction (x, y) {
+  console.log('scrollFunction x value = ' + x)
+  if (document.documentElement.scrollTop > 80) {
+    document.getElementById('shrinktext').style.height = x
+  } else {
+    document.getElementById('shrinktext').style.height = y
+  }
+}
 
-myFunction(); // Call listener function at run time
-fullscreen.addListener(myFunction); // Attach listener function on state changes
+window.onscroll = function () {
+  myFunction()
+}
+
+myFunction() // Call listener function at run time
+fullscreen.addListener(myFunction) // Attach listener function on state changes
